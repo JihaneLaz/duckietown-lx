f3e69ecd0541dd0d5a3cca12d21dcbd95d32a329
jihane <jihane3laz.jl@gmail.com>
2024-11-25 14:17:16 +0100

    tp2

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,8 +7,15 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+     
+    res[int(shape[0]/2):, 0:int(shape[1]/2)] = 1
+    res[ 0:int(shape[0]/2):,int(shape[1]/4):int(shape[1]/2)] = 1
+    res[int(shape[0]/2): , int(shape[1]/2):] = -1
+    res[ 0:int(shape[0]/2):,int(shape[1]/2):int(3*shape[1]/4)] = -1
+
+    res[ 0:int(shape[0]/2):,:int(shape[1]/4)] = -0.5
+    res[ 0:int(shape[0]/2):,int(shape[1]/4)*3:] = 0.5
+    #res[300:, 200:] = 1
     # ---
     return res
 
@@ -17,6 +24,12 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:300] = -1
-    # ---
+    res[int(shape[0]/2):, 0:int(shape[1]/2)] = -1
+    res[ 0:int(shape[0]/2):,int(shape[1]/4):int(shape[1]/2)] = -1
+    res[int(shape[0]/2): , int(shape[1]/2):] = 1
+    res[ 0:int(shape[0]/2):,int(shape[1]/2):int(3*shape[1]/4)] = 1
+
+    res[ 0:int(shape[0]/2):,:int(shape[1]/4)] = 0.5
+    res[ 0:int(shape[0]/2):,int(shape[1]/4)*3:] = -0.5
     return res
+
